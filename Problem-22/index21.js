4ffb8e95b2c3bcc4584622d5f19fccd2895f57aa
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-05-28 20:05:13 +0800

    Used recursion instead

--- Problem-22/index21.js
+++ Problem-22/index21.js
@@ -2,13 +2,8 @@
 Flatten a nested array. You must account for varying levels of nesting.
 */
 function steamrollArray(arr) {
-   for (var i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])){
-           arr = [].concat(...arr); 
-         i--;
-        } 
-    }
-   return arr
+   arr = [].concat(...arr)
+   return arr.every(e => !Array.isArray(e)) ? arr : steamrollArray(arr);
   }
   
   steamrollArray([1, [2, 3, [2, [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[4]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]])
